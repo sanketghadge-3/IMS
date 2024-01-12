5967b57ccd86efa06ff7eecba04333496e3dfe18
sanketghadge-3 <153154396+sanketghadge-3@users.noreply.github.com>
2024-01-12 19:38:01 +0530

    Update email_pass.py

--- email_pass.py
+++ email_pass.py
@@ -1,2 +1,15 @@
-email_='ims11052003@gmail.com'
-pass_='vnrq rkru mabx svim'
+email_='use_your_email'# This mail just use for send  OTP mail to any person in IMS(Inventory Managment System)
+pass_='Password' #Don't use Your original password
+"""
+Step1: Go to google account 
+Step2: First of all on your 2 step verfication
+Step3: Search app password in search section 
+Step4: Then Click on App Password 
+step5: Create new password 
+       if you already done with  Step2 just verfie your account
+Step6: Just give the App name(Title it can be anything you can give)
+       then click on create
+Step7: Then copy the password from new  your screen
+       just paste in pass_  upper section  
+
+"""
